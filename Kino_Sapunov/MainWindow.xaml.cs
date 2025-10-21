d77879c056357d4be3cbd598f5a3e557fdd66947
Sapunov-danil <sapunov.daniil13@gmail.com>
2025-10-21 21:55:28 +0500

    Настроил страницу MainWindow и создал метод перехода между страницами. Сапунов

--- Kino_Sapunov/MainWindow.xaml.cs
+++ Kino_Sapunov/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kino_Sapunov.Pages;
+using Mysqlx.Notice;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +17,32 @@ using System.Windows.Shapes;
 
 namespace Kino_Sapunov
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
+        public enum Pages 
+        {
+            kinoteatr,
+            afisha
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+        }
+
+        public void OpenPages(Pages page)
+        {
+            switch (page)
+            {
+                case Pages.kinoteatr:
+                    frame.Navigate(new KinoteatrPage());
+                    break;
+                case Pages.afisha:
+                    frame.Navigate(new AfishaPage());
+                    break;
+            }
         }
     }
 }
